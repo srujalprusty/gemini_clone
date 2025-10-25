38f5c8a70552a16b05df674bb13c5498fc2e4fe2
SRUJAL PRUSTY <s.srujal0612@gmail.com>
2025-10-26 01:35:11 +0530

    const changes

--- src/consts.js
+++ src/consts.js
@@ -1,1 +1,1 @@
-export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAoP8xTmeMlJuubMMjXh9gFy7J8ZiWWJDg"
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="
